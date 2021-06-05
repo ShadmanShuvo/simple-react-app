e8aec3cceb54984d196881a295da6a00420bd0ed
ShadmanShuvo <smsis404@gmail.com>
2021-06-05 19:04:49 +0600

    Use props and destructure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Card from "./components/Card";
 function App() {
     return <div>
         <h1 className="headingStyle">Todo App</h1>
-        <Card />
+        <Card titleText="Title One" descText="Description One" />
+        <Card titleText="Title Two" descText="Description Two" />
+        <Card titleText="Title Three" descText="Description Three" />
     </div>
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-const todoTitle = "Call Family";
-const todoDesc = "This is a card section for todo list items. From here you can get the proper idea."
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const currentYear = date.getFullYear();
 
-function Card() {
+function Card(props) {
+    const { titleText, descText } = props;
     return <div className="card">
-        <h3 className="cardTitle">{todoTitle}</h3>
-        <p className="cardDesc">{todoDesc}</p>
+        <h3 className="cardTitle">{titleText}</h3>
+        <p className="cardDesc">{descText}</p>
         <p className="cardFooter">{dateName + "/" + monthName + "/" + currentYear}</p>
     </div>
 }
