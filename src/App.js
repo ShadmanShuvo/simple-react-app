9c7635b4f1de305492f7b2d501054606cc7ebb42
ShadmanShuvo <smsis404@gmail.com>
2021-06-05 20:12:55 +0600

    Create json file and mapping data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "./components/Card";
+import Data from "./data.json";
 
 function App() {
     return <div>
         <h1 className="headingStyle">Todo App</h1>
-        <Card titleText="Title One" descText="Description One" />
-        <Card titleText="Title Two" descText="Description Two" />
-        <Card titleText="Title Three" descText="Description Three" />
+        {
+            Data.map((item, index) => <Card key={index} titleText={item.title} descText={item.desc} />)
+        }
     </div>
 }
 
